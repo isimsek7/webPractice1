bef3a45a79708f2516966dc4c293c211fd5452e1
isimsek7 <isimsekmusic@gmail.com>
2024-09-05 11:36:53 +0300

    Add files via upload

---
+++ WebPratik/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+var app = builder.Build();
+
+
+app.MapControllerRoute(
+    name:"Default",
+    pattern: "{controller=Home}/{action=Index}"
+    );
+
+app.Run();
+
